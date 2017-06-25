63f429e0c5ad7c693a033556edcd6948c9a34637
justaugy84 <guy@perimarketing.net>
2017-06-25 15:39:11 +0300

    added download buttons

--- docs/js/main.js
+++ docs/js/main.js
@@ -73,7 +73,97 @@ window.onload = function () {
     // Restart
     cropper.destroy();
     cropper = new Cropper(image, options);
-  }
+  };
+  // Methods
+  download.querySelector('.download-button').onclick = function (event) {
+    var e = event || window.event;
+    var target = e.target || e.srcElement;
+    var result;
+    var input;
+    var data;
+
+    if (!cropper) {
+      return;
+    }
+
+    while (target !== this) {
+      if (target.getAttribute('data-method')) {
+        break;
+      }
+
+      target = target.parentNode;
+    }
+
+    if (target === this || target.disabled || target.className.indexOf('disabled') > -1) {
+      return;
+    }
+
+    data = {
+      method: target.getAttribute('data-method'),
+      target: target.getAttribute('data-target'),
+      option: target.getAttribute('data-option'),
+      secondOption: target.getAttribute('data-second-option')
+    };
+
+    if (data.method) {
+      if (typeof data.target !== 'undefined') {
+        input = document.querySelector(data.target);
+
+        if (!target.hasAttribute('data-option') && data.target && input) {
+          try {
+            data.option = JSON.parse(input.value);
+          } catch (e) {
+            console.log(e.message);
+          }
+        }
+      }
+
+      if (data.method === 'getCroppedCanvas') {
+        data.option = JSON.parse(data.option);
+      }
+
+      result = cropper[data.method](data.option, data.secondOption);
+
+      switch (data.method) {
+        case 'scaleX':
+        case 'scaleY':
+          target.setAttribute('data-option', -data.option);
+          break;
+
+        case 'getCroppedCanvas':
+          if (result) {
+
+            // Bootstrap's Modal
+            $('#getCroppedCanvasModal').modal().find('.modal-body').html(result);
+
+            if (!download.disabled) {
+              download.href = result.toDataURL('image/jpeg');
+            }
+          }
+
+          break;
+
+        case 'destroy':
+          cropper = null;
+
+          if (uploadedImageURL) {
+            URL.revokeObjectURL(uploadedImageURL);
+            uploadedImageURL = '';
+            image.src = originalImageURL;
+          }
+
+          break;
+      }
+
+      if (typeof result === 'object' && result !== cropper && input) {
+        try {
+          input.value = JSON.stringify(result);
+        } catch (e) {
+          console.log(e.message);
+        }
+      }
+    }
+  };
 
   document.getElementById('file-name').innerHTML = imageName;
   document.getElementById('file-size').innerHTML = 'Original size: ' + imageWidth + 'x' + imageHeight;
