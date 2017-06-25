9f55ba466d4e5c7ee23e24148843659ebfe9a8b7
justaugy84 <guy@perimarketing.net>
2017-06-25 12:55:24 +0300

    fixed js errors

--- test/js/main.js
+++ test/js/main.js
@@ -8,13 +8,8 @@ window.onload = function () {
   var image = container.getElementsByTagName('img').item(0);
   var download = document.getElementById('download');
   var actions = document.getElementById('left-side');
-  var dataX = document.getElementById('dataX');
-  var dataY = document.getElementById('dataY');
   var dataHeight = document.getElementById('dataHeight');
   var dataWidth = document.getElementById('dataWidth');
-  var dataRotate = document.getElementById('dataRotate');
-  var dataScaleX = document.getElementById('dataScaleX');
-  var dataScaleY = document.getElementById('dataScaleY');
   var options = {
         aspectRatio: 16 / 9,
         zoomable: false,
@@ -35,13 +30,8 @@ window.onload = function () {
           var data = e.detail;
 
           console.log(e.type);
-          dataX.value = Math.round(data.x);
-          dataY.value = Math.round(data.y);
           dataHeight.value = Math.round(data.height);
           dataWidth.value = Math.round(data.width);
-          dataRotate.value = typeof data.rotate !== 'undefined' ? data.rotate : '';
-          dataScaleX.value = typeof data.scaleX !== 'undefined' ? data.scaleX : '';
-          dataScaleY.value = typeof data.scaleY !== 'undefined' ? data.scaleY : '';
         },
         zoom: function (e) {
           console.log(e.type, e.detail.ratio);
