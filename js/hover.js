5b99bacdedf650412a67ee113b13a3666020021d
justaugy84 <guy@perimarketing.net>
2017-07-02 18:40:47 +0300

    add icons to extention and constant search for image object on hover

--- js/hover.js
+++ js/hover.js
@@ -1,27 +1,31 @@
 var i;
+iconURL = chrome.extension.getURL("/icons/button-icon.png");
 
-if (document.readyState == "complete"){
-	var imagesList = document.images;
-	for (i = 0; i < imagesList.length; i++) {
-		addCropLink(imagesList[i]);
+// check elements mouse is hover
+document.addEventListener("mouseover", addCropLink, true);
+document.addEventListener("mouseout", removeCropLink, true);
+
+// add crop link if element is image
+function addCropLink(){
+	var target = event.target;
+	if (target instanceof HTMLImageElement){
+		var hoverDiv = document.createElement("div");
+		hoverDiv.setAttribute("class", "resizeMyPhoto");
+		hoverDiv.setAttribute("id", "resizeMyPhoto");
+		var cropLink = document.createElement("a");
+		cropLink.innerHTML += "Crop/Resize";
+		cropLink.href = chrome.extension.getURL("index.html#");
+		cropLink.href = cropLink.href + target.src
+		hoverDiv.appendChild(cropLink);
+		target.parentNode.insertBefore(hoverDiv, target.nextSibling);
 	}
 }
-else{
-	document.onreadystatechange = function () {
-		if (document.readyState == "complete"){
-			var imagesList = document.images;
-			for (i = 0; i < imagesList.length; i++) {
-				addCropLink(imagesList[i]);
-			}
-		}
-	}
-}
 
-function addCropLink(image){
-	var hoverDiv = document.createElement("div");
-	var cropLink = document.createElement("a");
-	cropLink.href = chrome.extension.getURL("index.html#");
-	cropLink.href = cropLink.href + image.src
-	hoverDiv.appendChild(cropLink);
-	image.parentNode.insertBefore(hoverDiv, image.nextSibling);
+// remove crop link if element is image
+function removeCropLink(){
+	var target = event.target;
+	if (target instanceof HTMLImageElement){
+		var hoverDiv = document.getElementById("resizeMyPhoto");
+		hoverDiv.parentNode.removeChild(hoverDiv);
+	}
 }
