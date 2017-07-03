2ecad4cb6e3d2a87c98b8809e720f4cd67beed09
justaugy84 <guy@perimarketing.net>
2017-07-03 13:06:17 +0300

    finished imagehover
    added web resources to fix link open in about:blank
    changed link position to top
    fixed removing / adding link onhover

--- js/hover.js
+++ js/hover.js
@@ -1,32 +1,39 @@
-var i;
 iconURL = chrome.extension.getURL("/icons/button-icon.png");
 
 // check elements mouse is hover
-document.addEventListener("mouseover", addCropLink, true);
-document.addEventListener("mouseout", removeCropLink, true);
+document.addEventListener("mouseover", setLink, true);
 
-// add crop link if element is image
-function addCropLink(){
+// handles creating of the crop link
+function setLink(){
 	var target = event.target;
 	if (target instanceof HTMLImageElement){
-		var cropLink = document.createElement("a");
-		cropLink.setAttribute("class", "resizeMyPhoto");
-		cropLink.setAttribute("id", "resizeMyPhoto");
-		cropLink.setAttribute("target", "_blank");
-		cropLink.innerHTML += "Crop/Resize";
-		cropLink.href = chrome.extension.getURL("index.html#");
-		cropLink.href = cropLink.href + target.src;
-		target.parentNode.style.position = "relative";
-		target.parentNode.style.display = "inline-block";
-		target.parentNode.insertBefore(cropLink, target.nextSibling);
-	}
-}
-
-// remove crop link if element is image
-function removeCropLink(){
-	var target = event.target;
-	if (target instanceof HTMLImageElement){
+		// make sure no links are visible
 		var cropLink = document.getElementById("resizeMyPhoto");
-		cropLink.parentNode.removeChild(cropLink);
+		if (cropLink !== null){
+			cropLink.parentNode.removeChild(cropLink);
+		}
+		// create the link
+		else{
+			cropLink = document.createElement("a");
+			cropLink.setAttribute("class", "resizeMyPhoto");
+			cropLink.setAttribute("id", "resizeMyPhoto");
+			cropLink.setAttribute("target", "_blank");
+			cropLink.innerHTML += "Crop/Resize";
+			cropLink.href = chrome.extension.getURL("index.html#") + target.src;
+			target.parentNode.style.position = "relative";
+			target.parentNode.style.display = "inline-block";
+			target.parentNode.insertBefore(cropLink, target.nextSibling);
+		}
 	}
+	else{
+		var cropLink = document.getElementById("resizeMyPhoto");
+		// make sure mouse is not on link
+		if(target == cropLink || cropLink == null){
+			return;
+		}
+		// remove the link
+		else{
+			cropLink.parentNode.removeChild(cropLink);
+		}
+	} 
 }
