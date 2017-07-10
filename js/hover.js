8184729aff901cbc6d313f5c93eec45a3465cc24
justaugy84 <guy@perimarketing.net>
2017-07-10 16:21:40 +0300

    more improvment to toggle feature and cleaned code

--- js/hover.js
+++ js/hover.js
@@ -1,5 +1,3 @@
-iconURL = chrome.extension.getURL("/icons/button-icon.png");
-
 // check extention status
 chrome.runtime.sendMessage({status: "getStatus"}, function(response) {
 	if (response.status == 'true'){
@@ -7,10 +5,6 @@ chrome.runtime.sendMessage({status: "getStatus"}, function(response) {
     	// check elements mouse is hover
 		document.addEventListener("mousemove", setLink, true);
 	}
-	else{
-		document.body.removeChild(cropperExtension.link);
-   		document.removeEventListener("mousemove", setLink, true);
-   	}
 });
 
 // wait for massage from background script
@@ -44,7 +38,6 @@ function setLinkVisibility(shouldShow) {
     link.style.top = (pos.top + 10) + "px";
     link.style.left = (pos.left + 10) + "px"
     link.href = chrome.extension.getURL("index.html#") + cropperExtension.curImage.src; 
-    console.log("showing link",pos);
   }
   link.style.display = displayValue;
 }
@@ -57,58 +50,11 @@ function getPosition(el) {
 }
 function setLink(e){
 	if (e.srcElement.nodeName === "IMG" && e.srcElement !== cropperExtension.curImage && e.srcElement.width >= 150) {
-    	console.log("new image hover", e.srcElement);
 	    cropperExtension.curImage = e.srcElement;
     	setLinkVisibility (true);
   	} else if (e.srcElement.nodeName !== "IMG" && e.srcElement !== cropperExtension.link) {
     	//we are off the image and off the link, hide the link
-	    console.log("out of image")
     	cropperExtension.curImage = null;
     	setLinkVisibility(false);
   	}
 }
-/*
-// handles creating of the crop link
-function setLink(){
-	var target = event.target;
-	if (target instanceof HTMLImageElement && target.width >= 150){
-		// make sure no links are visible
-		var cropLink = document.getElementById("resizeMyPhoto");
-		var cropDiv = document.getElementById("resizeMyPhotoDiv");
-		if (cropDiv !== null){
-			var fragment = document.createDocumentFragment();
-			fragment.appendChild(cropDiv.firstElementChild);
-			cropDiv.removeChild(cropLink);
-			cropDiv.parentNode.replaceChild(fragment, cropDiv);
-		}
-		// create the link
-		cropDiv = document.createElement("div");
-		cropDiv.setAttribute("id", "resizeMyPhotoDiv");
-		target.parentNode.appendChild(cropDiv);
-		cropDiv.appendChild(target);
-		cropLink = document.createElement("a");
-		cropLink.setAttribute("class", "resizeMyPhoto");
-		cropLink.setAttribute("id", "resizeMyPhoto");
-		cropLink.setAttribute("target", "_blank");
-		cropLink.innerHTML += "Crop/Resize";
-		cropLink.href = chrome.extension.getURL("index.html#") + target.src;
-		cropLink.style.marginTop = target.style.marginTop;
-		cropDiv.appendChild(cropLink);
-		cropDiv.appendChild(cropLink);
-	}
-	else{
-		var cropLink = document.getElementById("resizeMyPhoto");
-		var cropDiv = document.getElementById("resizeMyPhotoDiv");
-		// make sure mouse is not on link
-		if(target === cropLink || cropLink === null){
-			return;
-		}
-		// remove the link
-		else{
-			var fragment = document.createDocumentFragment();
-			fragment.appendChild(cropDiv.firstElementChild);
-			cropDiv.removeChild(cropLink);
-			cropDiv.parentNode.replaceChild(fragment, cropDiv);
-		}
-	}
-}*/
